316d59de2e4ad93cad5a8953f0793368de2bf806
Adriana Vars <asvutah@gmail.com>
2018-12-03 20:59:50 -0700

    add ranking

---
+++ db/migrate/20181204034044_add_ranking_to_songs.rb
@@ -0,0 +1,5 @@
+class AddRankingToSongs < ActiveRecord::Migration[5.2]
+  def change
+    add_column :songs, :rank, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_040053) do
+ActiveRecord::Schema.define(version: 2018_12_04_034044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_11_30_040053) do
     t.datetime "updated_at", null: false
     t.bigint "artist_id"
     t.bigint "billboard_id"
+    t.integer "rank"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
     t.index ["billboard_id"], name: "index_songs_on_billboard_id"
   end
