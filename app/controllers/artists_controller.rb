ce1eb19972e25f69e36dfa56e214bfdb45f79b95
Adriana Vars <asvutah@gmail.com>
2018-12-05 17:59:09 -0700

    update layout and gems

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,5 @@
 class ArtistsController < ApplicationController
-  # before_action :set_billboard 
+  before_action :set_billboard 
   before_action :set_artist, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -45,9 +45,9 @@ class ArtistsController < ApplicationController
     def artist_params
       params.require(:artist).permit(:name)
     end 
-    # def set_billboard
-    #   @billboard = Billboard.find(params[:billboard_id])
-    # end 
+    def set_billboard
+      @billboard = Billboard.find(params[:billboard_id])
+    end 
 
   def set_artist
     @artist = Artist.find(params[:id])
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
-# 10.times do
-#   artist = Artist.create(name: Faker::Music.band) 
-#   b = Billboard.create(name: Faker::Hobbit.character)
+50.times do
+  artist = Artist.create(name: Faker::Music.band, billboard_id: 39) 
+  # b = Billboard.create(name: Faker::Hobbit.character)
 
-#   10.times do
-#     Song.create(
-#       name: Faker::Vehicle.model,
-#       artist_id: artist.id,
-#       billboard_id: b.id
-#     )
-#   end 
-# end 
+50.times do
+    Song.create(
+      name: Faker::Vehicle.model,
+      rank: rand(10),
+      artist_id: artist.id,
+      billboard_id: 39
+    )
+  end 
+end 
 
 # Billboard.create(name: "Top100")
 # Billboard.create(name: "Top50")
